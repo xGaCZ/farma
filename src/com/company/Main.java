b5bb1a30a480ec0943db81b5dfd705897cfe809c
karolM <karmas999@wp.pl>
2020-09-15 17:54:20 +0200

    menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
-
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Scanner scan = new Scanner(System.in);
+
+        int rok = 2020;
+        int tydzien = 1;
+        System.out.println("Witaj w symulatorze bycia rolnikiem!!!");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("ZASADY SĄ PROSTE ");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Zaczynasz mając tylko 16k PLN \nMożesz kupować zwierzęta ziemie nasiona ziemia jest w hektarach\nCeny oraz wydajność są podane za hektar lub równowartośc w plonach za hektar ");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("JESLI CHCESZ ZACZĄĆ WCIŚNIJ ENTER");
+        System.in.read();
+
+        System.out.println("Dziś mamy: " + tydzien + " Tydzień " + rok + " Roku.");
+       Scanner in = new Scanner(System.in);
     }
 }
